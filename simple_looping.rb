45d387a769ccf8c361bcb8ce58725baaa68b3236
Nicolas Bermudez <bermnicolas@gmail.com>
2019-01-21 16:47:41 +0000

    Done.

--- simple_looping.rb
+++ simple_looping.rb
@@ -8,35 +8,53 @@ def loop_iterator(number_of_times)
   #   correct number of times?
   # Maybe we should keep count of the number of times we've puts out the 
   #  phrase and break when the counter hits the appropriate number...
-  phrase = "Welcome to Flatiron School's Web Development Course!"
-  
+  counter=0
+  loop do
+    counter += 1
+    phrase = "Welcome to Flatiron School's Web Development Course!"
+    puts phrase
+    break if number_of_times == counter
+  end
 end
 
 def times_iterator(number_of_times)
   # code your solution here using the "times" keyword
-  phrase = "Welcome to Flatiron School's Web Development Course!"
-  
+  number_of_times.times do
+    phrase = "Welcome to Flatiron School's Web Development Course!"
+    puts phrase
+  end
 end
 
 def while_iterator(number_of_times)
   # code your solution here using the "while" keyword
   # hint: user a counter to tell the while loop when to stop!
-  phrase = "Welcome to Flatiron School's Web Development Course!"
-  
+  counter = 0 
+  while number_of_times > counter
+    counter+=1
+    phrase = "Welcome to Flatiron School's Web Development Course!"
+    puts phrase
+  end
 end
 
 def until_iterator(number_of_times)
   # code your solution here using the "until" keyword
   # hint: use a counter to tell the until loop when to stop!
-  phrase = "Welcome to Flatiron School's Web Development Course!"
-
+  counter = 0 
+  until number_of_times == counter
+    counter+=1
+    phrase = "Welcome to Flatiron School's Web Development Course!"
+    puts phrase
+  end
 end
 
 def for_iterator(number_of_times)
   # code your solution here using the "for" keyword
   # remember that `for` requires a range of numbers. How can we turn our number into a range?
-  
-  phrase = "Welcome to Flatiron School's Web Development Course!"
+  range = 1..number_of_times
+  for number in range
+    phrase = "Welcome to Flatiron School's Web Development Course!"
+    puts phrase
+  end
   
 end
 
